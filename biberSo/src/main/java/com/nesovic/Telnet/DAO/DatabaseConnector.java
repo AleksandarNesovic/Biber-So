8f00b5e436addd63a270f90de0ecd8b8745982c7
AleksandarNesovic <crazynorth203@gmail.com>
2018-08-08 10:57:08 +0200

    heroku postgresql database deploy

--- biberSo/src/main/java/com/nesovic/Telnet/DAO/DatabaseConnector.java
+++ biberSo/src/main/java/com/nesovic/Telnet/DAO/DatabaseConnector.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DatabaseConnector {
 	
 	private static String driver="org.postgresql.Driver";
-	private static String url="jdbc:postgresql://localhost:5432/OrdersDB";
-	private static String username="postgres";
-	private static String password="qqq";
+	private static String url="jdbc:postgresql://ec2-54-217-235-159.eu-west-1.compute.amazonaws.com:5432/d5utn7s4s8d8d2";
+	private static String username="iqkhtgkxrvoogl";
+	private static String password="ce9c09756b23f004f5fbf776c6e9f5c2cea270c957660abcebf60bfc9dce1b94";
 	
 	public static Connection connect() throws ClassNotFoundException, SQLException {
 		Class.forName(driver);
